1862157e290c59bb2609ed2af40db82d2e84d79a
Nils Becker <info@nils-becker.com>
2013-06-28 17:30:23 +0200

    - added spinner to menu bar - removed refresh button from menu bar

--- src/com/nils/becker/fhplaner/controller/ScheduleListViewActivity.java
+++ src/com/nils/becker/fhplaner/controller/ScheduleListViewActivity.java
@@ -1,5 +1,7 @@
 package com.nils.becker.fhplaner.controller;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -10,22 +12,24 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.nils.becker.fhplaner.R;
 import com.nils.becker.fhplaner.helper.CourseAdapter;
 import com.nils.becker.fhplaner.helper.FetchScheduleTask;
-import com.nils.becker.fhplaner.R;
 import com.nils.becker.fhplaner.helper.ScheduleFetcher;
 import com.nils.becker.fhplaner.model.Course;
 import com.nils.becker.fhplaner.settings.SettingsActivity;
 
-public class ScheduleListViewActivity extends Activity implements ScheduleFetcher, SharedPreferences.OnSharedPreferenceChangeListener {
+public class ScheduleListViewActivity extends Activity implements ScheduleFetcher, SharedPreferences.OnSharedPreferenceChangeListener, ActionBar.OnNavigationListener {
 
 	private ArrayList<Course> list = new ArrayList<Course>();
 	private CourseAdapter adapter;
@@ -46,15 +50,14 @@ public class ScheduleListViewActivity extends Activity implements ScheduleFetche
         setContentView(R.layout.activity_schedule_list);
 
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        sharedPreferences.edit().clear().commit();
+        // sharedPreferences.edit().clear().commit();
 
         if (isFirstStartup()) {
             Intent switchToSettingsActivity = new Intent(ScheduleListViewActivity.this, SettingsActivity.class);
             startActivity(switchToSettingsActivity);
         }
 
-        ActionBar actionBar = getActionBar();
-        actionBar.setDisplayShowHomeEnabled(true);
+        setupActionBar();
 
 		new FetchScheduleTask(this).execute("http://nils-becker.com/fhg/schedule/lecturer/FW");
 
@@ -78,6 +81,34 @@ public class ScheduleListViewActivity extends Activity implements ScheduleFetche
         PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
     }
 
+    private void setupActionBar() {
+        final ActionBar actionBar = getActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE dd. MMM");
+        Calendar cal = Calendar.getInstance();
+
+        final String today = dateFormat.format(cal.getTime());
+        cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+
+        dateFormat = new SimpleDateFormat("EEE. dd");
+        final String startOfWeek = dateFormat.format(cal.getTime());
+        cal.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY);
+        final String endOfWeek= dateFormat.format(cal.getTime());
+
+        final String[] dropdownValues = {today, startOfWeek + " - " + endOfWeek};
+
+        ArrayAdapter<String> menuListAdapter = new ArrayAdapter<String>(actionBar.getThemedContext(),
+                android.R.layout.simple_spinner_item, android.R.id.text1,
+                dropdownValues);
+
+        menuListAdapter .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        actionBar.setListNavigationCallbacks(menuListAdapter , this);
+    }
+
     private Boolean isFirstStartup() {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         return sharedPreferences.getString("ressourceURL", "").equals("");
@@ -89,9 +120,9 @@ public class ScheduleListViewActivity extends Activity implements ScheduleFetche
         mItem = item;
 
         switch (item.getItemId()){
-            case R.id.action_refresh:
+    /*        case R.id.action_refresh:
                 System.out.println("refresh");
-                break;
+                break;*/
             case R.id.action_settings:
                 System.out.println("settings");
                 Intent showNextActivityIntent = new Intent(ScheduleListViewActivity.this, SettingsActivity.class);
@@ -122,5 +153,10 @@ public class ScheduleListViewActivity extends Activity implements ScheduleFetche
         if (s.equals("ressourceURL")) {
             new FetchScheduleTask(this).execute(sharedPreferences.getString("ressourceURL", ""));
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(int i, long l) {
+        return false;
     }
 }
